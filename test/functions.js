c1d87f6410dd512b4cc3ab5f4a87c2681d39c5e8
Marcelo <marcelo.martin@softvision.com>
2020-10-29 16:50:50 -0300

    more tests

--- test/functions.js
+++ test/functions.js
@@ -3,23 +3,11 @@ const axios = require('axios')
 const contacts = [{
     "id": 3,
     "name": "pocho",
-    "alias": "elPochoDeLaGente",
-    "phone": "123455663",
-    "imageUrl": "pocho.jpg",
-    "contactos": [1, 2, 4, 5, 6],
-    "createdAt": "2020-10-20T23:45:08.391Z",
-    "updatedAt": "2020-10-20T23:45:08.391Z",
     "deletedAt": null
 },
 {
     "id": 4,
     "name": "tito",
-    "alias": "elTitoDeLaGente",
-    "phone": "123455664",
-    "imageUrl": "tito.jpg",
-    "contactos": [1, 2, 3, 5, 6],
-    "createdAt": "2020-10-20T23:45:21.396Z",
-    "updatedAt": "2020-10-20T23:45:21.396Z",
     "deletedAt": null
 }]
 
@@ -43,7 +31,7 @@ const functions = {
             }
         })
         if (!result.length) {
-            result.push({ message: "Invalid Contact Id"})
+            result.push({ message: "Invalid Contact Id" })
         }
         return Promise.resolve(result);
     }
--- test/functions.test.js
+++ test/functions.test.js
@@ -1,6 +1,7 @@
 const functions = require('./functions');
 const setSuccessResponse = require('../dist/helpers/success-response-setters');
-const setFailResponse = require('../dist/helpers/fail-response-setter');;
+const setFailResponse = require('../dist/helpers/fail-response-setter');
+require('jest-extended');
 
 test('first id of fetched contacts should be 3', async () => {
     expect.assertions(1);
@@ -20,25 +21,109 @@ test('when passing an invalid id number as argument, status should be "error"', 
         .then(contacts => expect(contacts.status).toEqual('error'))
 })
 
-test('should return mocked contacts', () => {
-    functions.fetchMockedContacts()
-        .then(contacts => expect(contacts.length).toEqual(2))
+test('should return mocked contacts', async () => {
+    await functions.fetchMockedContacts()
+        .then(contacts => expect(contacts).toHaveLength(2))
 })
+
+test('contacts should be array', async () => {
+    await functions.fetchMockedContacts()
+        .then(contacts => expect(contacts).toBeArray())
+})
+
 
 test('status should be "success"', async () => {
     await functions.fetchMockedContactsById(3)
         .then(contact => setSuccessResponse(contact))
-        .then(response => expect(response.status).toMatch('success'))
+        .then(response => expect(response.status).toEqual('success'))
+})
+
+test('contact should have property id', async () => {
+    await functions.fetchMockedContactsById(3)
+        .then(contact => setSuccessResponse(contact))
+        .then(response => expect(response.data[0]).toHaveProperty('id'))
 })
 
 test('data length should be 1', async () => {
     await functions.fetchMockedContactsById(3)
         .then(contact => setSuccessResponse(contact))
-        .then(response => expect(response.data.length).toEqual(1))
+        .then(response => expect(response.data).toHaveLength(1))
+})
+
+test('contact id should be a number', async () => {
+    await functions.fetchMockedContactsById(3)
+        .then(contact => setSuccessResponse(contact))
+        .then(response => expect(response.data[0].id).toBeNumber())
+})
+
+test('contact should match object in test', async () => {
+    await functions.fetchMockedContactsById(3)
+        .then(contact => setSuccessResponse(contact))
+        .then(response => expect(response.data[0]).toMatchObject(
+            {
+                "id": 3,
+                "name": "pocho",
+                "deletedAt": null
+            }
+        ))
+})
+
+test('contact should not have property "phone"', async () => {
+    await functions.fetchMockedContactsById(3)
+        .then(contact => setSuccessResponse(contact))
+        .then(response => expect(response.data[0]).not.toHaveProperty('phone'))
+})
+
+test('contact should be an object', async () => {
+    await functions.fetchMockedContactsById(3)
+        .then(contact => setSuccessResponse(contact))
+        .then(response => expect(response.data[0]).toBeObject())
+})
+
+test('contact should have keys id, name and deletedAt', async () => {
+    await functions.fetchMockedContactsById(3)
+        .then(contact => setSuccessResponse(contact))
+        .then(response => expect(response.data[0]).toContainKeys(['id', 'name', 'deletedAt']))
+})
+
+test('deletedAt field should be falsy', async () => {
+    await functions.fetchMockedContactsById(3)
+        .then(contact => setSuccessResponse(contact))
+        .then(response => expect(response.data[0].deletedAt).toBeFalsy())
+})
+
+test('deletedAt field should be null', async () => {
+    await functions.fetchMockedContactsById(3)
+        .then(contact => setSuccessResponse(contact))
+        .then(response => expect(response.data[0].deletedAt).toBeNull())
+})
+
+test('response message should be a string', async () => {
+    await functions.fetchMockedContactsById(100)
+        .then(message => setFailResponse([message[0]]))
+        .then(response => expect(response.data[0].message).toBeString());
+})
+
+test('response message should be "Invalid Contact Id"', async () => {
+    await functions.fetchMockedContactsById(100)
+        .then(message => setFailResponse([message[0]]))
+        .then(response => expect(response.data[0]).toHaveProperty("message"))
 })
 
 test('response message should be "Invalid Contact Id"', async () => {
     await functions.fetchMockedContactsById(100)
         .then(message => setFailResponse([message[0]]))
         .then(response => expect(response.data[0].message).toEqual("Invalid Contact Id"))
+})
+
+test('response message should be "Invalid Contact Id"', async () => {
+    await functions.fetchMockedContactsById(100)
+        .then(message => setFailResponse([message[0]]))
+        .then(response => expect(response.data[0].message).toBeDefined())
+})
+
+test('response message should be "Invalid Contact Id"', async () => {
+    await functions.fetchMockedContactsById(100)
+        .then(message => setFailResponse([message[0]]))
+        .then(response => expect(response.data[0]).not.toHaveProperty("id"))
 })
